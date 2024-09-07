0549e8269f6f1245b744dc40fa28822f8b901b11
Elikem Buertey <ebuertey@uwaterloo.ca>
2024-09-07 12:12:30 -0400

    added files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
